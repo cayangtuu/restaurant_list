ab9458ad2047d46fd787c8b30ad892acabcd4c26
cayang <cayang@simenvi.com.tw>
2022-12-07 14:25:59 +0800

    add category search

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require("express")
 const exphbs = require("express-handlebars")
 const restaurantList = require("./restaurant.json").results
 
+function categoriesList() {
+  const categories = []
+  restaurantList.forEach(restaurant => {
+    if (!categories.includes(restaurant.category)) {
+      categories.push(restaurant.category)
+    }
+  })
+  return categories
+}
+
 const app = express()
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }))
 app.set("view engine", "hbs")
@@ -9,7 +19,7 @@ app.set("view engine", "hbs")
 app.use(express.static('public'))
 
 app.get("/", (req, res) => {
-  res.render("index", { restaurants: restaurantList })
+  res.render("index", { restaurants: restaurantList, categories: categoriesList() })
 })
 
 app.get("/restaurants/:id", (req, res) => {
@@ -20,8 +30,11 @@ app.get("/restaurants/:id", (req, res) => {
 
 app.get("/search", (req, res) => {
   const keyword = req.query.keyword
-  const restaurantSearch = restaurantList.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()))
-  res.render("index", { restaurants: restaurantSearch })
+  const category = req.query.category
+  const restaurantSearch = restaurantList.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) && restaurant.category.includes(category)
+  })
+  res.render("index", { restaurants: restaurantSearch, categories: categoriesList() })
 })
 
 const port = 3000
