17ce88c8da1fa645409794790bb31f971755eebc
cayang <cayang@simenvi.com.tw>
2022-12-12 17:05:34 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
+const mongoose = require("mongoose")
 const restaurantList = require("./restaurant.json").results
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
 
 
 // 取得不重複之餐廳類別陣列
