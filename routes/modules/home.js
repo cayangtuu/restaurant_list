a67f5c5be5e2e36c7b4c022d49713d6442a1f345
LAPTOP-QJBRI58J\Anne <koala1995610@gmail.com>
2022-12-18 21:09:40 +0800

    add JS standard style

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,37 +3,37 @@ const router = express.Router()
 const Restaurants = require("../../models/Restaurant")
 
 let categoryList = function () {
-    return Restaurants.find()
-      .lean()
-      .then(restaurant => {
-        return restaurant
-          .map(restaurant => restaurant.category)
-          .filter((item, index, arr) => {
-            return arr.indexOf(item) === index;
-          });
-      })
+  return Restaurants.find()
+    .lean()
+    .then(restaurant => {
+      return restaurant
+        .map(restaurant => restaurant.category)
+        .filter((item, index, arr) => {
+          return arr.indexOf(item) === index;
+        });
+    })
 }
 
 router.get("/", (req, res) => {
-    Restaurants.find()
-      .lean()
-      .then(restaurant => {
-        categoryList().then(categories => {
-          res.render("index", { restaurant, categories })
-        })
+  Restaurants.find()
+    .lean()
+    .then(restaurant => {
+      categoryList().then(categories => {
+        res.render("index", { restaurant, categories })
       })
-      .catch(error => {
-        console.log(error)
-        res.render("error", { "errmsg": error.message })
-      })
+    })
+    .catch(error => {
+      console.log(error)
+      res.render("error", { "errmsg": error.message })
+    })
 })
 
 router.get("/search", (req, res) => {
-    const keyword = req.query.keyword
-    const category = req.query.category
-    Restaurants.find()
-      .lean()
-      .then(restaurant => {
+  const keyword = req.query.keyword
+  const category = req.query.category
+  Restaurants.find()
+    .lean()
+    .then(restaurant => {
       const restaurantSearch = restaurant
         .filter(restaurant => {
           return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) &&
@@ -41,12 +41,12 @@ router.get("/search", (req, res) => {
         });
       categoryList().then(categories => {
         res.render("index", { restaurant: restaurantSearch, categories, keyword, category })
-        })
       })
-      .catch(error => {
-        console.log(error)
-        res.render("error", { "errmsg": error.message })
-      })
+    })
+    .catch(error => {
+      console.log(error)
+      res.render("error", { "errmsg": error.message })
+    })
 })
 
 module.exports = router
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -3,88 +3,88 @@ const router = express.Router()
 const Restaurants = require("../../models/Restaurant")
 
 let categoryList = function () {
-    return Restaurants.find()
-      .lean()
-      .then(restaurant => {
-        return restaurant
-          .map(restaurant => restaurant.category)
-          .filter((item, index, arr) => {
-            return arr.indexOf(item) === index;
-          });
-      })
+  return Restaurants.find()
+    .lean()
+    .then(restaurant => {
+      return restaurant
+        .map(restaurant => restaurant.category)
+        .filter((item, index, arr) => {
+          return arr.indexOf(item) === index;
+        });
+    })
 }
 
 router.get("/new", (req, res) => {
-    categoryList()
-      .then(categories => {
-        res.render("new", { categories })
-      })
-      .catch(error => {
-        console.log(error)
-        res.render("error", { "errmsg": error.message })
-      })
+  categoryList()
+    .then(categories => {
+      res.render("new", { categories })
+    })
+    .catch(error => {
+      console.log(error)
+      res.render("error", { 'errmsg': error.message })
+    })
 })
-  
+
 router.post("/", (req, res) => {
-    Restaurants.create(req.body)
-      .then(() => res.redirect("/"))
-      .catch(error => {
-        console.log(error)
-        res.render("error", { "errmsg": error.message })
-      })
+  Restaurants.create(req.body)
+    .then(() => res.redirect("/"))
+    .catch(error => {
+      console.log(error)
+      res.render("error", { 'errmsg': error.message })
+    })
 })
-  
+
 router.get("/:id", (req, res) => {
-    const id = req.params.id
-    Restaurants.findById(id)
-      .lean()
-      .then(restaurant => res.render("show", { restaurant }))
-      .catch(error => {
-        console.log(error)
-        res.render("error", { "errmsg": error.message })
-      })
+  const id = req.params.id
+  Restaurants.findById(id)
+    .lean()
+    .then(restaurant => res.render("show", { restaurant }))
+    .catch(error => {
+      console.log(error)
+      res.render("error", { 'errmsg': error.message })
+    })
 })
-  
+
 router.get("/:id/edit", (req, res) => {
-    const id = req.params.id
-    Restaurants.findById(id)
-      .lean()
-      .then(restaurant => {
-        categoryList().then(categories => {
-          res.render("edit", { restaurant, categories })
-        })
+  const id = req.params.id
+  Restaurants.findById(id)
+    .lean()
+    .then(restaurant => {
+      categoryList().then(categories => {
+        res.render("edit", { restaurant, categories })
       })
-      .catch(error => {
-        console.log(error)
-        res.render("error", { "errmsg": error.message })
-      })
+    })
+    .catch(error => {
+      console.log(error)
+      res.render("error", { 'errmsg': error.message })
+    })
 })
 
 router.put("/:id", (req, res) => {
-    const id = req.params.id
-    Restaurants.findById(id)
-      .then(restaurant => {
-        for (const item in req.body) {
-          restaurant[item] = req.body[item]
-        }
-        return restaurant.save()
-      })
-      .then(() => res.redirect(`/restaurants/${id}`))
-      .catch(error => {
-        console.log(error)
-        res.render("error", { "errmsg": error.message })
-      })
+  const id = req.params.id
+  Restaurants.findById(id)
+    .then(restaurant => {
+      for (const item in req.body) {
+        restaurant[item] = req.body[item]
+      }
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => {
+      console.log(error)
+      res.render("error", { 'errmsg': error.message })
+    })
 })
-  
+
 router.delete("/:id", (req, res) => {
-    const id = req.params.id
-    Restaurants.findById(id)
-      .then(restaurant => restaurant.remove())
-      .then(() => res.redirect("/"))
-      .catch(error => {
-        console.log(error)
-        res.render("error", { "errmsg": error.message })
-      })
+  const id = req.params.id
+  Restaurants.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect("/"))
+    .catch(error => {
+      console.log(error)
+      res.render("error", { 'errmsg': error.message })
+    })
 })
 
 module.exports = router
