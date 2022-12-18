e925ceed74c2e54ff0f9b494deadf610db811524
LAPTOP-QJBRI58J\Anne <koala1995610@gmail.com>
2022-12-18 17:12:48 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
-const mongoose = require("mongoose")
 const methodOverride = require("method-override")
 const routes = require("./routes")
-
-
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config()
-}
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require("./config/mongoose")
 
 const app = express()
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }))
--- models/seeds/RestaurantSeeders.js
+++ models/seeds/RestaurantSeeders.js
@@ -1,18 +1,9 @@
 const mongoose = require("mongoose")
 const restaurantModel = require("../Restaurant")
 const restaurantList = require("../../restaurant.json").results
+const db = require("../../config/mongoose")
 
-if (process.env.NODE_ENV !== 'production') {
-  require("dotenv").config()
-}
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
-  console.log('mongodb connected!')
   restaurantList.forEach(restaurant => {
     restaurantModel.create(restaurant)
   });
